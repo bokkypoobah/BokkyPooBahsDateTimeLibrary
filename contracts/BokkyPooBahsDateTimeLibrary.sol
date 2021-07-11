f2025cc027acd02d555cbd291bdbe5acf935819f
Alex Beregszaszi <alex@rtfs.hu>
2021-07-12 00:32:50 +0100

    Support Solidity 0.7.x/0.8.x
    For 0.8.x one could use unchecked blocks to save gas, nevertheless this works.

--- contracts/BokkyPooBahsDateTimeLibrary.sol
+++ contracts/BokkyPooBahsDateTimeLibrary.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
 
 // ----------------------------------------------------------------------------
 // BokkyPooBah's DateTime Library v1.01
